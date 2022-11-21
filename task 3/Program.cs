d7fae934826684cc72c7b36449ab2f14511d744a
Dinkaui <ninidzed@gmail.com>
2022-11-21 21:41:05 +0300

    task 15

---
+++ task 3/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, которая принимает на вход цифру, обозначающую день недели и проверяет, является ли этот день выходным
+Console.WriteLine("Введите цифру дня недели: ");
+int weekDayNum = int.Parse(Console.ReadLine());
+
+if (weekDayNum == 6 || weekDayNum ==7)
+{
+    Console.WriteLine("Weeeekeend!!!!");
+}
+
+else if (weekDayNum < 1 || weekDayNum > 7)
+{
+    Console.WriteLine("OOops incorrect number!!!!");
+}
+
+else 
+{
+    Console.WriteLine("Weekday!");
+}
